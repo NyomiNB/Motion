8fa05b9ffb83a26136fa666f3ad103082ff99b2a
AaroheeD <aaroheedabas@gmail.com>
2024-08-17 14:59:01 +0000

    Add inspiration, animate fish, change writing

---
+++ script.js
@@ -0,0 +1,28 @@
+var button = $(".add")
+var container = $(".toDoList")
+
+button.on("click", addTask)
+
+function addTask(event){
+  event.preventDefault()
+  var task = $(".task").val()
+  container.append(`<p>${task} <button class="delete"> Delete</button><p>`)
+  $(".task").val("")
+}
+
+container.on("click", deleteCard)
+
+//event handler
+function deleteCard(){
+  if (event.target.className === "delete"){
+    event.target.parentNode.remove()
+  }
+}
+
+// var image = $(".swimmingFish")
+
+// moveImage()
+
+// function moveImage(){
+//   image: right 100px;
+// }
